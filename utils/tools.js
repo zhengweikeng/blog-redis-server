10e6bc01541f6d54c50ffdfd55512d9e98c0c8fd
Wayne Zheng <seed_1029@outlook.com>
2016-04-30 10:40:03 +0800

    Chinese to pinyin, change Chinese punctuation to English punctuation

--- utils/tools.js
+++ utils/tools.js
@@ -1,11 +1,38 @@
 const crypto = require('crypto')
 const pinyin = require('pinyin')
+const punctuationMap = {
+  '，': ',', 
+  '。': '.', 
+  '、': ',', 
+  '《': '<', 
+  '》': '>', 
+  '？': '?', 
+  '；': ';', 
+  '：': ':',
+  '’': '\'',
+  '‘': '\'', 
+  '”': '"', 
+  '“': '"',
+  '【': '[', 
+  '】': ']', 
+  '｛': '{', 
+  '｝': '}',  
+  '～': '~',
+  '！': '!', 
+  '（': '(', 
+  '）': ')', 
+  '＝': '='
+}
 
 const md5 = (text) => {
   return crypto.createHash('md5').update(text).digest('hex')
 }
 
 const chinese2pinyin = (chineseText) => {
+  Object.keys(punctuationMap).forEach((cnPunc) => {
+    console.log(cnPunc)
+    chineseText = chineseText.replace(new RegExp(cnPunc, 'g'), punctuationMap[cnPunc])
+  })
   const pinyins = pinyin(chineseText, {style: pinyin.STYLE_NORMAL})
   return pinyins.reduce((prev, next) => (prev+next[0]).toString(), '')
 }
