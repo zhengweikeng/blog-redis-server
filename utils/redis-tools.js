2a11f4921343ad19294acb770bf7595a474d6c9b
zhengweikeng <seed_1029@outlook.com>
2016-05-10 19:07:02 +0800

    update

--- utils/redis-tools.js
+++ utils/redis-tools.js
@@ -5,24 +5,39 @@ const generateBaseInfoKey = (key) => {
   return `${redisKey.BASEINFO}#${key}`
 }
 
+const getAttr = (title, delimiters) => {
+  const start = title.indexOf(delimiters)
+  const end = title.lastIndexOf(delimiters)
+  if (start > 0 && end > 0 && end > start) {
+    return title.substring(start + 1, end)
+  }
+  return null
+}
+
 const generateBaseInfo = (title) => {
   const baseInfos = {}
   if (title.lastIndexOf('.md') > 0) {
     title = title.substring(0, title.indexOf('.md'))
     baseInfos.ext = 'md'
   }
-  const infos = title.split('#')
-  infos.forEach((info, i) => {
-    if (i === 0) {
-      baseInfos.title = info
-    }
-    if (i === 1) {
-      baseInfos.tag = info.split(',')
-    }
-    if (i === 2) {
-      baseInfos.postTime = info
-    }
-  })
+  
+  let pureTitle = title
+  
+  const tags = getAttr(title, '#')
+  if (tags) {
+    baseInfos.tag = tags.split(',')
+    const oriTag = `#${tags}#`
+    pureTitle = pureTitle.replace(oriTag, '')
+  }
+  
+  const postTime = getAttr(title, '$')
+  if (postTime) {
+    baseInfos.postTime = postTime
+    const oriPostTime = `$${postTime}$`
+    pureTitle = pureTitle.replace(oriPostTime, '')
+  }
+  
+  baseInfos.title = pureTitle
   
   return baseInfos
 }
